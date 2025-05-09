2650369fbe12bf3a8223c8899cd6f7793ae75fae
dexter <dexter.lente18@gmail.com>
2025-05-09 10:36:17 +0800

    added convo_id

--- internal/models/message.go
+++ internal/models/message.go
@@ -5,9 +5,10 @@ import (
 )
 
 type Message struct {
-    ID         int       `db:"id"`
-    SenderID   int       `db:"sender_id"`
-    ReceiverID int       `db:"receiver_id"`
-    Content    string    `db:"content"`
-    SentAt     time.Time `db:"sent_at"`
+	ID             int       `db:"id"`
+	ConversationID int       `db:"conversation_id"`
+	SenderID       int       `db:"sender_id"`
+	ReceiverID     int       `db:"receiver_id"`
+	Content        string    `db:"content"`
+	SentAt         time.Time `db:"sent_at"`
 }
