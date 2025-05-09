041825cf684090425e900d0103057a1922c73fdb
dexter <dexter.lente18@gmail.com>
2025-05-09 14:03:22 +0800

    added convo id

--- internal/db/migrations.go
+++ internal/db/migrations.go
@@ -9,52 +9,51 @@ import (
 
 // Run all migrations
 func runMigrations(db *sqlx.DB) error {
-    migrations := []func(*sqlx.DB) error{
-        migrateUsers,
-        migrateMessages,
-        // migrateProducts,  // Example additional table migration
-        // Add more migrations here
-    }
+	migrations := []func(*sqlx.DB) error{
+		migrateUsers,
+		migrateMessages,
+		// Add more migrations here
+	}
 
-    for _, migrate := range migrations {
-        if err := migrate(db); err != nil {
-            return err
-        }
-    }
-    log.Println("Migrations completed successfully!")
-    return nil
+	for _, migrate := range migrations {
+		if err := migrate(db); err != nil {
+			return err
+		}
+	}
+	log.Println("Migrations completed successfully!")
+	return nil
 }
 
 func tableExists(db *sqlx.DB, tableName string) (bool, error) {
-    var exists bool
-    query := `SELECT EXISTS (
+	var exists bool
+	query := `SELECT EXISTS (
         SELECT FROM information_schema.tables 
         WHERE table_schema = 'public' 
         AND table_name = $1
     )`
-    err := db.Get(&exists, query, tableName)
-    if err != nil {
-        return false, fmt.Errorf("failed to check if table exists: %v", err)
-    }
-    return exists, nil
+	err := db.Get(&exists, query, tableName)
+	if err != nil {
+		return false, fmt.Errorf("failed to check if table exists: %v", err)
+	}
+	return exists, nil
 }
 
 // Migrate Users Table
 func migrateUsers(db *sqlx.DB) error {
-    // Check if the table already exists
-    exists, err := tableExists(db, "users")
-    if err != nil {
-        return fmt.Errorf("failed to check if users table exists: %v", err)
-    }
+	// Check if the table already exists
+	exists, err := tableExists(db, "users")
+	if err != nil {
+		return fmt.Errorf("failed to check if users table exists: %v", err)
+	}
 
-    // If the table exists, print a message and return
-    if exists {
-        log.Println("Users table already exists!")
-        return nil
-    }
+	// If the table exists, print a message and return
+	if exists {
+		log.Println("Users table already exists!")
+		return nil
+	}
 
-    // Create the table if it doesn't exist
-    _, err = db.Exec(`
+	// Create the table if it doesn't exist
+	_, err = db.Exec(`
         CREATE TABLE users (
             id SERIAL PRIMARY KEY,
             first_name VARCHAR(100) NOT NULL,
@@ -66,42 +65,42 @@ func migrateUsers(db *sqlx.DB) error {
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     `)
-    if err != nil {
-        return fmt.Errorf("failed to create users table: %v", err)
-    }
+	if err != nil {
+		return fmt.Errorf("failed to create users table: %v", err)
+	}
 
-    log.Println("Users table created successfully!")
-    return nil
+	log.Println("Users table created successfully!")
+	return nil
 }
 
-
 func migrateMessages(db *sqlx.DB) error {
-    // Check if the table already exists
-    exists, err := tableExists(db, "messages")
-    if err != nil {
-        return fmt.Errorf("failed to check if messages table exists: %v", err)
-    }
+	// Check if the table already exists
+	exists, err := tableExists(db, "messages")
+	if err != nil {
+		return fmt.Errorf("failed to check if messages table exists: %v", err)
+	}
 
-    // If the table exists, print a message and return
-    if exists {
-        log.Println("messages table already exists!")
-        return nil
-    }
+	// If the table exists, print a message and return
+	if exists {
+		log.Println("messages table already exists!")
+		return nil
+	}
 
-    // Create the table if it doesn't exist
-    _, err = db.Exec(`
+	// Create the table if it doesn't exist
+	_, err = db.Exec(`
         CREATE TABLE  messages (
             id SERIAL PRIMARY KEY,
+            conversation_id INT NOT NULL,
             sender_id INT REFERENCES users(id) ON DELETE CASCADE,
             receiver_id INT REFERENCES users(id) ON DELETE CASCADE,
             content TEXT NOT NULL,
             sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     `)
-    if err != nil {
-        return fmt.Errorf("failed to create messages table: %v", err)
-    }
+	if err != nil {
+		return fmt.Errorf("failed to create messages table: %v", err)
+	}
 
-    log.Println("Messages table created successfully!")
-    return nil
+	log.Println("Messages table created successfully!")
+	return nil
 }
